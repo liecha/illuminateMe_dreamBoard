801405fd72f3118a5b9ad1864a0edd330173429b
emchju <liecha.jutvik@gmail.com>
2024-11-09 10:27:54 +0100

    Took away main

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
-from data.connection import main
 
 #######################
 # Page configuration
@@ -72,7 +71,6 @@ st.markdown("""
 
 #######################
 # Load data
-main()
 df_energy = pd.read_csv('data/energy-irl-results.csv')
 
 #######################
